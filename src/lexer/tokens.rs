8e05cf74e86b200fb6fd12398388cb641a62b2cc
Gerardo Torres Castro <gtr@gerardotorres.me>
2023-09-19 09:51:34 -0400

    lexer, errors: update lexer and add tests
    this commit updates the lexer to use 2 spaces for tabs, fixes a couple
    inconsistencies, and adds tests.

--- src/lexer/tokens.rs
+++ src/lexer/tokens.rs
@@ -1,111 +1,136 @@
 use std::fmt;
 
 /// Token represents a token in the ivy programming language.
-#[allow(dead_code)]
-#[derive(Debug)]
-pub enum Token {
-
-    // Atoms
-    Integer(i32),   // 32
-    String(String), // "hello"
-    Symbol(String), // user-defined
-    True,           // true
-    False,          // false
-    None,           // None
-
-    // Operators
-    Plus,           // +            
-    PlusPlus,       // ++
-    Minus,          // -
-    MinusMimus,     // --
-    Star,           // *
-    Slash,          // /
-    Bind,           // =
-    Eq,             // ==
-    Not,            // !
-    NotEq,          // !=
-    Greater,        // >
-    GreaterEqual,   // >=
-    Less,           // <
-    LessEqual,      // <=
-    And,            // &&
-    Or,             // ||
-
-    // Delimeters
-    Pipe,           // |
-    Arrow,          // ->
-    Comma,          // ,
-    Semicolon,      // ;
-    LParen,         // (
-    RParen,         // )
-    LBracket,       // [
-    RBracket,       // ]
-    LCurly,         // {
-    RCurly,         // }
-
-    // Keywords
-    Let,            // let
-    Fn,             // fn
-    If,             // if
-    Data,           // data
-    Enum,           // enum
-    Then,           // then
-    Else,           // else
-    Tuple,          // tuple 
-    Match,          // match
-    Print,          // print
-    PrintLn,        // println
+pub struct Token {
+  pub col: usize,
+  pub row: usize,
+  pub typ: TokenType,
 }
 
 impl fmt::Display for Token {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match &self {
-            Token::Integer(i) => write!(f, "Integer: {}", i),
-            Token::String(s) => write!(f, "String: {}", s),
-            Token::Symbol(s) => write!(f, "Symbol: {}", s),
-            Token::True => write!(f, "True"),
-            Token::False => write!(f, "False"),
-            Token::None => write!(f, "None"),
-            
-            Token::Plus => write!(f, "Plus"),
-            Token::PlusPlus => write!(f, "PlusPlus"),
-            Token::Minus => write!(f, "Minus"),
-            Token::MinusMimus => write!(f, "MinusMinus"),
-            Token::Star => write!(f, "Star"),
-            Token::Slash => write!(f, "Slash"),
-            Token::Bind => write!(f, "Bind"),
-            Token::Eq => write!(f, "Eq"),
-            Token::Not => write!(f, "Not"),
-            Token::NotEq => write!(f, "NotEq"),
-            Token::Greater => write!(f, "Greater"),
-            Token::GreaterEqual => write!(f, "GreaterEqual"),
-            Token::Less => write!(f, "Less"),
-            Token::LessEqual => write!(f, "LessEqual"),
-            Token::And => write!(f, "And"),
-            Token::Or => write!(f, "Or"),
-            
-            Token::Pipe => write!(f, "Pipe"),
-            Token::Arrow => write!(f, "Arrow"),
-            Token::Comma => write!(f, "Comma"),
-            Token::Semicolon => write!(f, "Semicolon"),
-            Token::LParen => write!(f, "LParen"),
-            Token::RParen => write!(f, "RParen"),
-            Token::LBracket => write!(f, "LBracket"),
-            Token::RBracket => write!(f, "RBracket"),
-            Token::LCurly => write!(f, "LCurly"),
-            Token::RCurly => write!(f, "RCurly"),
-            
-            Token::Let => write!(f, "Let"),
-            Token::Fn => write!(f, "Fn"),
-            Token::If => write!(f, "If"),
-            Token::Then => write!(f, "Then"),
-            Token::Else => write!(f, "Else"),
-            Token::Tuple => write!(f, "Tuple"),
-            Token::Data => write!(f, "Data"),
-            Token::Enum => write!(f, "Enum"),
-            Token::Match => write!(f, "Match"),
-            Token::Print => write!(f, "Print"),
-            Token::PrintLn => write!(f, "Println"),
-        }
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "({}, {})\t{}", self.row, self.col, self.typ)
+  }
+}
+
+#[allow(dead_code)]
+#[derive(Debug, PartialEq, Clone)]
+pub enum TokenType {
+  // Atoms
+  Integer(i32),   /* 42           */
+  String(String), /* "hello"      */
+  Symbol(String), /* user-defined */
+  None,           /* None         */
+
+  // Operators
+  Plus,           /* +  */    Minus,      /* -  */
+  PlusPlus,       /* ++ */    Star,       /* *  */
+  Slash,          /* /  */    Bind,       /* =  */
+  Eq,             /* == */    Not,        /* !  */
+  NotEq,          /* != */    Greater,    /* >  */
+  GreaterEqual,   /* >= */    Less,       /* <  */
+  LessEqual,      /* <= */    And,        /* && */
+  Or,             /* || */
+
+  // Delimeters
+  Bar,            /*  |  */   Arrow,          /*  -> */
+  EqArrow,        /*  => */   Dot,            /*  .  */
+  Comma,          /*  ,  */   Colon,          /*  :  */
+  Semicolon,      /*  ;  */   DoubleColon,    /*  :: */
+  LParen,         /*  (  */   RParen,         /*  )  */
+  LBracket,       /*  [  */   RBracket,       /*  ]  */
+  LCurly,         /*  {  */   RCurly,         /*  }  */
+  CommentLeft,    /*  (- */   CommentRight,   /*  -) */
+  At,             /*  @  */   Comment,        /*  -- */
+
+  // Keywords
+  Let,            /* let     */   Mut,        /* mut     */
+  Fn,             /* fn      */   If,         /* if      */
+  Then,           /* then    */   Else,       /* else    */
+  Pub,            /* pub     */   Data,       /* data    */
+  Struct,         /* struct  */   Package,    /* package */
+  Import,         /* import  */   Match,      /* match   */
+  With,           /* with    */   While,      /* while   */   
+  Do,             /* do      */   Return,     /* return  */
+  Trait,          /* trait   */   Impl,       /* impl    */
+  For,            /* for     */
+
+  // Decorators
+  Decorator(String),
+
+  EOF,            /* end of file */
+}
+
+impl fmt::Display for TokenType {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    match &self {
+      // Atoms
+      TokenType::Integer(i)   => write!(f, "Integer '{}'", i),
+      TokenType::String(s) => write!(f, "String '{}'", s),
+      TokenType::Symbol(s) => write!(f, "Symbol '{}'", s),
+      TokenType::None               => write!(f, "None"),
+      
+      // Operators
+      TokenType::Plus         => write!(f, "+"),
+      TokenType::PlusPlus     => write!(f, "++"),
+      TokenType::Minus        => write!(f, "-"),
+      TokenType::Star         => write!(f, "*"),
+      TokenType::Slash        => write!(f, "/"),
+      TokenType::Bind         => write!(f, "="),
+      TokenType::Eq           => write!(f, "=="),
+      TokenType::Not          => write!(f, "!"),
+      TokenType::NotEq        => write!(f, "!="),
+      TokenType::Greater      => write!(f, ">"),
+      TokenType::GreaterEqual => write!(f, ">="),
+      TokenType::Less         => write!(f, "<"),
+      TokenType::LessEqual    => write!(f, "<="),
+      TokenType::And          => write!(f, "&&"),
+      TokenType::Or           => write!(f, "||"),
+      
+      // Delimeters
+      TokenType::Bar          => write!(f, "|"),
+      TokenType::EqArrow      => write!(f, "=>"),
+      TokenType::Arrow        => write!(f, "->"),
+      TokenType::Comma        => write!(f, ","),
+      TokenType::Dot          => write!(f, "."),
+      TokenType::Semicolon    => write!(f, ";"),
+      TokenType::Colon        => write!(f, ":"),
+      TokenType::DoubleColon  => write!(f, "::"),
+      TokenType::LParen       => write!(f, "("),
+      TokenType::RParen       => write!(f, ")"),
+      TokenType::LBracket     => write!(f, "["),
+      TokenType::RBracket     => write!(f, "]"),
+      TokenType::LCurly       => write!(f, "{{"),
+      TokenType::RCurly       => write!(f, "}}"),
+      TokenType::Comment      => write!(f, "--"),
+      TokenType::CommentLeft  => write!(f, "(-"),
+      TokenType::CommentRight => write!(f, "-)"),
+      TokenType::At           => write!(f, "@"),
+      
+      // Keywords
+      TokenType::Let          => write!(f, "let"),
+      TokenType::Fn           => write!(f, "fn"),
+      TokenType::If           => write!(f, "if"),
+      TokenType::Then         => write!(f, "then"),
+      TokenType::Else         => write!(f, "else"),
+      TokenType::Pub          => write!(f, "pub"),
+      TokenType::Mut          => write!(f, "mut"),
+      TokenType::Data         => write!(f, "Data"),
+      TokenType::Import       => write!(f, "import"),
+      TokenType::Package      => write!(f, "package"),
+      TokenType::Struct       => write!(f, "struct"),
+      TokenType::Match        => write!(f, "match"),
+      TokenType::With         => write!(f, "with"),
+      TokenType::While        => write!(f, "while"),
+      TokenType::Do           => write!(f, "do"),
+      TokenType::Return       => write!(f, "return"),
+      TokenType::Trait        => write!(f, "trait"),
+      TokenType::Impl         => write!(f, "impl"),
+      TokenType::For          => write!(f, "for"),
+      
+      TokenType::Decorator(s) => write!(f, "Decorator: {}", s),
+
+      TokenType::EOF          => write!(f, "EOF")
     }
+  }
 }
