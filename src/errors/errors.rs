19e47313bfdb34c2e32776c7ccda7bcad3c865d9
Gerardo Torres Castro <gtr@gerardotorres.me>
2023-09-21 18:10:00 -0400

    *: switch remaining files to two spaces
    I missed some files while transitioning to two spaces.

--- src/errors/errors.rs
+++ src/errors/errors.rs
@@ -14,191 +14,189 @@ const PARSER_ERR: &str  = "Parser error";
 
 /// Holds the two types of lexer errors.
 enum LexerErrType {
-    InvalidToken(char),
-    UnterminatedString,
+  InvalidToken(char),
+  UnterminatedString,
 }
 
 impl fmt::Display for LexerErrType {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let msg = match *self {
-            LexerErrType::UnterminatedString => format!("Unterminated string"),
-            LexerErrType::InvalidToken(car) => format!("Invalid Token: `{car}`"),
-        };
-        write!(f, "{ERR_START}{LEXER_ERR}{ERR_END}: {msg}")
-    }
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    let msg = match *self {
+      LexerErrType::UnterminatedString => format!("Unterminated string"),
+      LexerErrType::InvalidToken(car) => format!("Invalid Token: `{car}`"),
+    };
+    write!(f, "{ERR_START}{LEXER_ERR}{ERR_END}: {msg}")
+  }
 }
 
 /// Holds the two types of parser errors.
+#[derive(Debug)]
 enum ParserErrType {
-    Expected(String),
-    ExpecetedOneOf(Vec<String>),
+  Expected(String),
+  ExpecetedOneOf(Vec<String>),
 }
 
 impl fmt::Display for ParserErrType {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let msg = match self {
-            ParserErrType::Expected(car) => format!("Expected {}", car.clone()),
-            ParserErrType::ExpecetedOneOf(car) => {
-                let mut comma = false;
-                let mut one_of = String::new();
-                for tok in car {
-                    if comma {
-                        one_of += ", ";
-                    }
-                    one_of += &tok;
-                    comma = true;
-                }
-                format!("Expected one of: {one_of}")
-            },
-        };
-        write!(f, "{ERR_START}{PARSER_ERR}{ERR_END}: {msg}")
-    }
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    let msg = match self {
+      ParserErrType::Expected(car) => format!("Expected {}", car.clone()),
+      ParserErrType::ExpecetedOneOf(car) => {
+        let mut comma = false;
+        let mut one_of = String::new();
+        for tok in car {
+          if comma {
+            one_of += ", ";
+          }
+          one_of += &tok;
+          comma = true;
+        }
+        format!("Expected one of: {one_of}")
+      },
+    };
+    write!(f, "{ERR_START}{PARSER_ERR}{ERR_END}: {msg}")
+  }
 }
 
 pub struct LexerError {
-    row: usize,
-    col: usize,
-    typ: LexerErrType,
+  row: usize,
+  col: usize,
+  typ: LexerErrType,
 }
 
 /// Creates a LexerError struct for an invalid token error.
 pub fn new_invalid_token(tok: char, row: usize, col: usize) -> LexerError {
-    LexerError { row, col, typ: LexerErrType::InvalidToken(tok) }
+  LexerError { row, col, typ: LexerErrType::InvalidToken(tok) }
 }
 
 /// Creates a LexerError struct for an untermianted string error.
 pub fn new_unterminated_str(row: usize, col: usize) -> LexerError {
-    LexerError { row, col, typ: LexerErrType::UnterminatedString }
+  LexerError { row, col, typ: LexerErrType::UnterminatedString }
 }
 
 
+#[derive(Debug)]
 pub struct ParserError {
-    row: usize,
-    col: usize,
-    typ: ParserErrType,
+  row: usize,
+  col: usize,
+  typ: ParserErrType,
 }
 
 pub fn new_parser_expected(row: usize, col: usize, val: String) -> ParserError {
-    ParserError { row, col, typ: ParserErrType::Expected(val) }
+  ParserError { row, col, typ: ParserErrType::Expected(val) }
 }
 
 pub fn new_parser_expected_one_of(
-    row: usize, col: usize, val: Vec<String>
+  row: usize, col: usize, val: Vec<String>
 ) -> ParserError {
-    ParserError { row, col, typ: ParserErrType::ExpecetedOneOf(val) }
+  ParserError { row, col, typ: ParserErrType::ExpecetedOneOf(val) }
 }
 
 impl LexerError {
-    pub fn show_error(&self, src: &str, input: &str) {
-        let padding = get_padding(self.row);
-        
-        self.show_error_title();
-        self.show_error_source(input, padding);
-        self.show_code_snippet(padding, src);
-    }
+  pub fn show_error(&self, src: &str, input: &str) {
+    let padding = get_padding(self.row);
 
-    fn show_error_title(&self) {
-        let title = format!("{}", self.typ);
-        println!("\n{title}");
-    }
+    self.show_error_title();
+    self.show_error_source(input, padding);
+    self.show_code_snippet(padding, src);
+  }
 
-    fn show_error_source(&self, input: &str, padding: usize) {
-        let indent = " ".repeat(padding);
-        let (row, col) = (self.row, self.col);
-        print!("{indent}{ERR_START}{DOWN_RIGHT}{HRZT}{HRZT}[");
-        println!("{input} ({row}:{col})]{ERR_END}")
-    }
+  fn show_error_title(&self) {
+    let title = format!("{}", self.typ);
+    println!("\n{title}");
+  }
 
-    fn show_code_snippet(&self, padding: usize, src: &str) {
-        let indent = " ".repeat(padding);
-        let arrow_offset = " ".repeat(self.col);
-        let left_bars = "─".repeat(padding);
-        let row = self.row;
-        let line = get_line(src, self.row);
-        println!("{indent}{ERR_START}{VERT}{ERR_END}");
-        println!(" {ERR_START}{row}{VERT} {ERR_END}{line}");
-        println!("{indent}{ERR_START}{VERT}{arrow_offset}^");
-        println!("{ERR_START}{left_bars}{UP_LEFT}");
-        
-        println!("{ERR_END}");
-    }
+  fn show_error_source(&self, input: &str, padding: usize) {
+    let indent = " ".repeat(padding);
+    let (row, col) = (self.row, self.col);
+    print!("{indent}{ERR_START}{DOWN_RIGHT}{HRZT}{HRZT}[");
+    println!("{input} ({row}:{col})]{ERR_END}")
+  }
 
-    
+  fn show_code_snippet(&self, padding: usize, src: &str) {
+    let indent = " ".repeat(padding);
+    let arrow_offset = " ".repeat(self.col);
+    let left_bars = "─".repeat(padding);
+    let row = self.row;
+    let line = get_line(src, self.row);
+    println!("{indent}{ERR_START}{VERT}{ERR_END}");
+    println!(" {ERR_START}{row}{VERT} {ERR_END}{line}");
+    println!("{indent}{ERR_START}{VERT}{arrow_offset}^");
+    println!("{ERR_START}{left_bars}{UP_LEFT}");
+
+    println!("{ERR_END}");
+  }
 }
 
 
 impl ParserError {
-    pub fn show_error(&self, src: &str, input: &str) {
-        let padding = get_padding(self.row);
-        
-        self.show_error_title();
-        self.show_error_source(input, padding);
-        self.show_code_snippet(padding, src);
-    }
+  pub fn show_error(&self, src: &str, input: &str) {
+    let padding = get_padding(self.row);
 
-    fn show_error_title(&self) {
-        let title = format!("{}", self.typ);
-        println!("\n{title}");
-    }
+    self.show_error_title();
+    self.show_error_source(input, padding);
+    self.show_code_snippet(padding, src);
+  }
 
-    fn show_error_source(&self, input: &str, padding: usize) {
-        let indent = " ".repeat(padding);
-        let (row, col) = (self.row, self.col);
-        print!("{indent}{ERR_START}{DOWN_RIGHT}{HRZT}{HRZT}[");
-        println!("{input} ({row}:{col})]{ERR_END}")
-    }
+  fn show_error_title(&self) {
+    let title = format!("{}", self.typ);
+    println!("\n{title}");
+  }
 
-    fn show_code_snippet(&self, padding: usize, src: &str) {
-        let indent = " ".repeat(padding);
-        let arrow_offset = " ".repeat(self.col);
-        let left_bars = "─".repeat(padding);
-        let row = self.row;
-        let line = get_line(src, self.row);
-        println!("{indent}{ERR_START}{VERT}{ERR_END}");
-        println!(" {ERR_START}{row}{VERT} {ERR_END}{line}");
-        println!("{indent}{ERR_START}{VERT}{arrow_offset}^");
-        println!("{ERR_START}{left_bars}{UP_LEFT}");
-        
-        println!("{ERR_END}");
-    }
+  fn show_error_source(&self, input: &str, padding: usize) {
+    let indent = " ".repeat(padding);
+    let (row, col) = (self.row, self.col);
+    print!("{indent}{ERR_START}{DOWN_RIGHT}{HRZT}{HRZT}[");
+    println!("{input} ({row}:{col})]{ERR_END}")
+  }
 
-    
+  fn show_code_snippet(&self, padding: usize, src: &str) {
+    let indent = " ".repeat(padding);
+    let arrow_offset = " ".repeat(self.col);
+    let left_bars = "─".repeat(padding);
+    let row = self.row;
+    let line = get_line(src, self.row);
+    println!("{indent}{ERR_START}{VERT}{ERR_END}");
+    println!(" {ERR_START}{row}{VERT} {ERR_END}{line}");
+    println!("{indent}{ERR_START}{VERT}{arrow_offset}^");
+    println!("{ERR_START}{left_bars}{UP_LEFT}");
+
+    println!("{ERR_END}");
+  }   
 }
 
 /// Returns the number of characters a row number takes up.
 /// e.g. row `34` takes up 2 characters.
 fn get_line_size(row: usize) -> usize {
-    (0..).take_while(|i| 10u64.pow(*i) <= row.try_into().unwrap()).count()
+  (0..).take_while(|i| 10u64.pow(*i) <= row.try_into().unwrap()).count()
 }
 
 /// Given a row number, returns how much padding should be added to the left of
 /// the error message.
 fn get_padding(row: usize) -> usize {
-    1 + get_line_size(row)
+  1 + get_line_size(row)
 }
 
 fn get_line(src: &str, row: usize) -> String {
-    let mut line = String::new();
-    let acc_row = row - 1;
-    let mut cur_row = 0;
-    let mut ptr = 0;
-    
-    while acc_row != cur_row {
-        if src.chars().nth(ptr) == Some('\n') {
-            cur_row += 1;
-        }
-        ptr += 1;
-    }
+  let mut line = String::new();
+  let acc_row = row - 1;
+  let mut cur_row = 0;
+  let mut ptr = 0;
 
-    let mut ended = false;
-    while !ended {
-        match src.chars().nth(ptr) {
-            Some('\n')  => ended = true,
-            Some(col)   => line.push(col),
-            _           => break,
-        }
-        ptr += 1;
+  while acc_row != cur_row {
+    if src.chars().nth(ptr) == Some('\n') {
+      cur_row += 1;
     }
+    ptr += 1;
+  }
 
-    return line;
+  let mut ended = false;
+  while !ended {
+    match src.chars().nth(ptr) {
+      Some('\n')  => ended = true,
+      Some(col)   => line.push(col),
+      _           => break,
+    }
+    ptr += 1;
+  }
+
+  return line;
 }
--- src/repl/repl.rs
+++ src/repl/repl.rs
@@ -5,6 +5,7 @@ use crate::parser::ast::*;
 
 use rustyline::{Cmd, Editor, EventHandler, KeyCode, KeyEvent, Modifiers};
 use rustyline::error::ReadlineError;
+use crate::parser::printer::*;
 
 const INDENT: &str = "  ";
 const PROMPT: &str = "\x1b[92mivy>\x1b[0m ";
@@ -19,85 +20,85 @@ const TYPE_MSG: &str = "displays the type of <expr>";
 const EXPR_MSG: &str = "evaluates <expr>";
 
 fn print_repl_message() {
-    println!("The ivy programming language ({})", VERSION);
-	println!("enter :quit to quit.");
+  println!("The ivy programming language ({})", VERSION);
+  println!("enter :quit or :q to quit.");
 }
 
 /// Handles commands of the form of `:cmd`. These are mainly having to do with 
 /// modifying or viewing the REPL environment.
 fn handle_command(cmd: String) {
-    match cmd.as_str() {
-        ":l" | ":list" => {
-            println!("List of commands available from the Ivy REPL:\n");
-            println!("{INDENT}:                 {REPT_MSG}");
-            println!("{INDENT}:list             {LIST_MSG}");
-            println!("{INDENT}:load <package>   {LOAD_MSG}");
-            println!("{INDENT}:tree <expr>      {TREE_MSG}");
-            println!("{INDENT}:type <expr>      {TYPE_MSG}");
-            println!("");
-            println!("{INDENT}<expr>            {EXPR_MSG}");
-        },
-        ":t" | ":type"  => {
-            println!("{TYPE_MSG}");
-        },
-        ":load" => {
-            println!("{LOAD_MSG}");
-        },
-        ":" => {
-            println!("{REPT_MSG}");
-        },
-        _ => {
-            println!("Unrecognized command. Enter `:list` for a list of available commands.");
-        }
+  match cmd.as_str() {
+    ":l" | ":list" => {
+      println!("List of commands available from the Ivy REPL:\n");
+      println!("{INDENT}:                 {REPT_MSG}");
+      println!("{INDENT}:list             {LIST_MSG}");
+      println!("{INDENT}:load <package>   {LOAD_MSG}");
+      println!("{INDENT}:tree <expr>      {TREE_MSG}");
+      println!("{INDENT}:type <expr>      {TYPE_MSG}");
+      println!("");
+      println!("{INDENT}<expr>            {EXPR_MSG}");
+    },
+    ":t" | ":type"  => {
+      println!("{TYPE_MSG}");
+    },
+    ":load" => {
+      println!("{LOAD_MSG}");
+    },
+    ":" => {
+      println!("{REPT_MSG}");
+    },
+    _ => {
+      println!("Unrecognized command. Enter `:list` for a list of available commands.");
     }
+  }
 }
 
 pub fn repl() {
-    print_repl_message();
+  print_repl_message();
 
-    let mut rl = Editor::<()>::new();
-    if rl.load_history("history.txt").is_err() {};
+  let mut rl = Editor::<()>::new();
+  if rl.load_history("history.txt").is_err() {};
 
-    rl.bind_sequence(
-        KeyEvent(KeyCode::Tab, Modifiers::NONE),
-        EventHandler::Simple(Cmd::Insert(1, "\t".to_string()))
-    );
-    
-    loop {
-        let readline = rl.readline(PROMPT);
+  rl.bind_sequence(
+    KeyEvent(KeyCode::Tab, Modifiers::NONE),
+    EventHandler::Simple(Cmd::Insert(1, "\t".to_string()))
+  );
 
-        match readline {
-            Ok(line) => {
-                rl.add_history_entry(line.as_str());
-                if line == ":quit" {
-                    return;
-                }
+  loop {
+    let readline = rl.readline(PROMPT);
 
-                if line.chars().next().unwrap() == ':' {
-                    handle_command(line);
-                } else {
-                    let token_result = lex(&line, REPL);
-                    if token_result.is_some() {
-                        let tokens = token_result.unwrap();
-                        // for token in tokens {    
-                        //     println!("{} ", token);
-                        // }
-                        let ast = parse(tokens);
-                        match ast {
-                            Err(err) => {err.show_error(&line, REPL);}
-                            Ok(root) => {print_tree(root);}
-                        }
-                    }
-                }
-            },
-            Err(ReadlineError::Interrupted) | Err(ReadlineError::Eof)=> {
-                break
-            },
-            Err(err) => {
-                println!("Error: {:?}", err);
-                break
-            }
+    match readline {
+      Ok(line) => {
+        rl.add_history_entry(line.as_str());
+        if line == ":quit" || line == ":q"{
+          return;
         }
+
+        if line.chars().next().unwrap() == ':' {
+          handle_command(line);
+        } else {
+          let token_result = lex(&line, REPL);
+          if token_result.is_some() {
+            let tokens = token_result.unwrap();
+            // for token in tokens {
+            //     println!("{} ", token);
+            // }
+            let ast = parse(tokens);
+            match ast {
+              Err(err) => {err.show_error(&line, REPL);}
+              Ok(root) => {print_tree(root);}
+            }
+          }
+        }
+      },
+      Err(ReadlineError::Interrupted) | Err(ReadlineError::Eof)=> {
+        break
+      },
+      Err(err) => {
+        println!("Error: {:?}", err);
+        break
+      }
     }
-    rl.save_history("history.txt").unwrap();
+  }
+  rl.save_history("history.txt").unwrap();
 }
